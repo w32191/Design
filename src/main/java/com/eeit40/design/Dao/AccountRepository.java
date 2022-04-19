2adb9464e23849e58a9af846bf289bfd2e3123c1
andy <a9631514@gmail.com>
2022-04-19 14:59:40 +0800

    增加Dao 增加登入邏輯 跟 新增會員 邏輯

--- src/main/java/com/eeit40/design/Dao/AccountRepository.java
+++ src/main/java/com/eeit40/design/Dao/AccountRepository.java
@@ -6,10 +6,13 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountRepository extends JpaRepository<Account, Integer>{
+public interface AccountRepository {
 
-	public Optional<Account> findByEmail(String email);
-	
-	public Optional<Account> findBySalt(String salt);
+	public Integer update(Account account);
+
+	public Integer insert(Account account);
+
+	public Account findByemail(String email);
+
 
 }
--- src/main/java/com/eeit40/design/Service/Impl/AccountServiceImpl.java
+++ src/main/java/com/eeit40/design/Service/Impl/AccountServiceImpl.java
@@ -1,6 +1,7 @@
-package com.eeit40.design.Service;
+package com.eeit40.design.Service.Impl;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,6 +9,7 @@ import org.springframework.util.DigestUtils;
 
 import com.eeit40.design.Dao.AccountRepository;
 import com.eeit40.design.Entity.Account;
+import com.eeit40.design.Service.AccountService;
 import com.eeit40.design.utils.ValidFormat;
 import com.eeit40.design.vo.AccountVO;
 
@@ -20,10 +22,10 @@ public class AccountServiceImpl implements AccountService {
 	@Autowired
 	private AccountService accountService;
 	
-	private String getmd5pwd(String pwd , Account account) {
+	private String getmd5pwd(String pwd , String salt) {
 		
 		//對pwd+salt 進行三次加密
-		String str = pwd + account;
+		String str = pwd + salt;
 		for(int i = 0; i < 3; i++) {
 			str = DigestUtils.md5DigestAsHex(str.getBytes()).toUpperCase();
 		}
@@ -33,15 +35,20 @@ public class AccountServiceImpl implements AccountService {
 	@Override
 	public AccountVO login(Account account) {
 		
-		Optional<Account> optionsalt = accountDao.findBySalt(account.getSalt());
-		
 		//檢查帳號是否存在
-		Optional<Account> optionemail = accountDao.findByEmail(account.getEmail());
-		if(optionemail == null) {
+		Account accountemail = accountDao.findByemail(account.getEmail());
+		if(accountemail == null) {
 			return null;
 		}
 		
-		String md5pwd = getmd5pwd(account.getPwd(), optionsalt.get());
+		//使用資料庫鹽值對輸入密碼進行加密
+		String md5pwd = getmd5pwd(account.getPwd(), accountemail.getSalt());
+		
+		//比對密碼是否相等
+		if(!md5pwd.equals(accountemail.getPwd())) {
+			return null;
+		}
+		
 		
 		AccountVO accountvo = new AccountVO();
 		accountvo.setEmail(account.getEmail());
@@ -49,6 +56,8 @@ public class AccountServiceImpl implements AccountService {
 		return accountvo;
 	}
 	
+
+	@Override
 	public Optional<String> register(AccountVO accountvo){
 		
 		//驗證欄位是否填寫及格式相等
@@ -57,12 +66,24 @@ public class AccountServiceImpl implements AccountService {
 		if(!accountvo.getPwd().equals(accountvo.getCheckpwd())) return Optional.of("兩次輸入密碼不相符");
 		
 		//檢查帳號是否重複註冊
-		Optional<Account> option = accountDao.findByEmail(accountvo.getEmail());
-		if(option != null) {
-			return Optional.of("該帳號已被使用");
+		Account dataemail = accountDao.findByemail(accountvo.getEmail());
+		if(dataemail != null) {
+			return Optional.of("該帳號已被使用");	
 		}
-		return null;
 		
+		//產生鹽值
+		String salt = UUID.randomUUID().toString().toUpperCase().replaceAll("-","");
+		
+		// 新增MemberAccount 資料
+		Account account = new Account();
+		account.setEmail(accountvo.getEmail());
+		account.setPwd(accountvo.getPwd());
+		account.setSalt(salt);
+		Integer id = accountDao.insert(account);
+		if(id == 0) {
+			return Optional.of("新增會員時發生錯誤");
+		}
+		return Optional.empty();
 		
 	}
 
