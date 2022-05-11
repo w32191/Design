f7ccbd8266c86f99bb2633d70c39c94ec19ece44
王柏翔 <w32191@gmail.com>
2022-05-11 09:31:31 +0800

    編輯活動的預覽照片

--- src/main/webapp/static/back/universal/js/EditActivity.js
+++ src/main/webapp/static/back/universal/js/EditActivity.js
@@ -428,7 +428,7 @@ $(function () {
     if(input.files && input.files[0]){
       let reader = new FileReader();
       reader.onload = function (e) {
-        $("#activityImg").attr('src', e.target.result).removeAttr('hidden');
+        $("#activityImg").attr('src', e.target.result);
       }
       reader.readAsDataURL(input.files[0]);
     }
